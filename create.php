3a3949be0a07b977f24463b03bb73af9093cd65e
cris <cris.pijper@gmail.com>
2018-02-26 16:03:02 +0100

    added functionality, overview, item details

--- create.php
+++ create.php
@@ -9,13 +9,34 @@
     <div class="container">
       <h1>Create item</h1>
       <form method="post" action="create.php">
-       <p>Todo title: </p>
-       <input name="todoTitle" type="text">
-       <p>Todo description: </p>
-       <input name="todoDescription" type="text">
-       <br>
+       <label for="todoTitle">Title</label>
+       <input id="todoTitle" name="todoTitle" type="text" placeholder="enter your title"><br />
+       <label for="todoDescription">Notes</label>
+       <input id="todoDescription" name="todoDescription" type="text" placeholder="enter your notes">
+       <br><br>
        <input type="submit" name="submit" value="submit">
       </form>
     </div>
   </body>
 </html>
+
+<?php
+require_once("db_connect.php");
+
+if(isset($_POST['submit'])) {
+  $title = $_POST['todoTitle'];
+  $description = $_POST['todoDescription'];
+
+  // make connection to db
+  db();
+  global $link;
+  $query = "INSERT INTO todo(todoTitle, todoDescription, date) VALUES ('$title', '$description', now() )";
+  $insertTodo = mysqli_query($link, $query);
+  if($insertTodo){
+    echo "success!";
+  } else {
+    echo mysql_error($link);
+  }
+  mysqli_close($link);
+}
+ ?>
--- index.php
+++ index.php
@@ -1,13 +1,37 @@
+<?php require_once("db_connect.php"); ?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/4.0.0-beta.3/materia/bootstrap.min.css">
-    <title>Je moet connected zijn...</title>
+    <title>Todo list</title>
   </head>
   <body>
     <div class="container">
-      <h1>Index</h1>
+      <h2>Todo list</h2>
+      <?php
+      db();
+      global $link;
+      $query = "SELECT id, todoTitle, todoDescription, date FROM todo";
+      $result = mysqli_query($link, $query);
+      // check if there is data
+      if(mysqli_num_rows($result) >= 1){
+        while($row = mysqli_fetch_array($result)){
+          $id = $row['id'];
+          $title = $row['todoTitle'];
+          $date = $row['date'];
+      ?>
+      <ul class="list-group">
+        <li class="list-group-item"><a href="item.php?id=<?php echo $id ?>"><?php echo $title ?></a> [<?php echo $date ?>]</li>
+      </ul>
+      <?php
+        }
+      }
+      ?>
+
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button type="button" class="btn btn-primary" onclick="window.location.href='create.php'">Add item</button>
+      </nav>
     </div>
   </body>
 </html>
